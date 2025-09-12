1adee987387a833a861ee9d6256d6da1db556f61
cviets <cviets@mgh.harvard.edu>
2025-09-12 17:46:25 -0400

    Added Vercel analytics React component

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import './global.css'; // Assuming global.css is also inside the "pages" folder
+import { Analytics } from "@vercel/analytics/next"
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return ( 
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
