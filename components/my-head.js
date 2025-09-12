3bbfd2be61ceef618ba98cab936615c02a6a446a
cviets <cviets@mgh.harvard.edu>
2025-09-12 18:53:20 -0400

    Dynamic head image changing

--- components/my-head.js
+++ components/my-head.js
@@ -4,7 +4,10 @@ export default function MyHead({myText}) {
     return (
         <Head>
             <title>{myText}</title>
-            <link rel="icon" href="/flask-light.png"/>
+            <link rel="icon" href="/flask-dark.png" media="(prefers-color-scheme: light)" />
+            <link rel="icon" href="/flask-light.png" media="(prefers-color-scheme: dark)" />
+
+            <link rel="icon" href="/flask-with-bg.png" />
         </Head>
     );
 }
