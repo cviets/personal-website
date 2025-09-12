490f517161f454c95f3849c788d8fd82331616f8
cviets <cviets@mgh.harvard.edu>
2025-09-12 18:27:18 -0400

    Changed light/dark color scheme

--- components/my-head.js
+++ components/my-head.js
@@ -4,8 +4,8 @@ export default function MyHead({myText}) {
     return (
         <Head>
             <title>{myText}</title>
-            <link rel="icon" href="/flask-light.png" media="(prefers-color-scheme: dark)"/>
-            <link rel="icon" href="/flask-dark.png" media="(prefers-color-scheme: light)"/>
+            <link rel="icon" href="/flask-light.png" media="(prefers-color-scheme: light)"/>
+            <link rel="icon" href="/flask-dark.png" media="(prefers-color-scheme: dark)"/>
         </Head>
     );
 }
